584a6cfc319ea9939ba58efa1ded8961f5d61634
Syed Basim <social.syedbasim@gmail.com>
2022-02-18 15:13:28 +0300

    🚀 initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import TodoForm from './TodoForm.svelte';
+
+	let todos = [];
+
+	const addTodo = (event) => {
+		todos = [...todos, event.detail.todoContent];
+	};
+</script>
+
+<main>
+	Welcome to dummy todo app!
+
+	{#each todos as todo}
+		<p>{todo}</p>
+	{/each}
+
+	<TodoForm on:todoCreate={addTodo} />
+</main>
---
+++ src/TodoForm.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	let todoContent = '';
+
+	const handleSubmit = () => {
+		dispatch('todoCreate', { todoContent });
+	};
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<input type="text" bind:value={todoContent} />
+	<button>Add todo</button>
+</form>
